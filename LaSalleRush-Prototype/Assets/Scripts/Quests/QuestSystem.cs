6fa0c7fde8f19bc082e840d0a65c84cc7c48e331
janniehatesu <jannaarcullo@gmail.com>
2023-03-09 20:38:20 +0800

    level 4 to 6 (di pa tapos pero sana gumana pi)

--- LaSalleRush-Prototype/Assets/Scripts/Quests/QuestSystem.cs
+++ LaSalleRush-Prototype/Assets/Scripts/Quests/QuestSystem.cs
@@ -98,7 +98,7 @@ public class QuestSystem : MonoBehaviour
                     gameManagerscript.currentPassenger += 1; //currentpassenger = 3
                 }
         }
-         if(gameManagerscript.currentlevel == 3)
+        if(gameManagerscript.currentlevel == 3)
         {
             //4th passenger
             if(gameManagerscript.currentPassenger == 3)
@@ -116,8 +116,99 @@ public class QuestSystem : MonoBehaviour
                     spawnManagerscript.DropOffPoints[4].SetActive(true); // Gregoria Montoya Hall - dropoff Spawn
                     gameManagerscript.currentPassenger += 1; //currentpassenger = 5
                 }
+            
+            //6th passenger 
+            if(spawnManagerscript.DropOffPoints[4].activeSelf == false & gameManagerscript.currentlevel == 3 & gameManagerscript.currentPassenger == 5 ) //Check if dropoff prefab is not active, current level and current passenger is equal
+                {
+                    spawnManagerscript.PickupPoints[5].SetActive(false); //CBAA- pickup despawn
+                    spawnManagerscript.DropOffPoints[5].SetActive(true); //Ayuntamiento - dropoff Spawn
+                    gameManagerscript.currentPassenger += 1; //currentpassenger = 6
+                } 
         }
-           
+
+        if(gameManagerscript.currentlevel == 4)
+        {
+            //7th passenger
+            if(gameManagerscript.currentPassenger == 6)
+            {
+                spawnManagerscript.PickupPoints[0].SetActive(false); // Rotonda-pickup despawn
+                spawnManagerscript.DropOffPoints[6].SetActive(true); // Clothing warehouse-dropoff spawn
+                
+                gameManagerscript.currentPassenger += 1; //currentpassenger = 7
+            }
+            
+            //8th passenger 
+            if(spawnManagerscript.DropOffPoints[6].activeSelf == false & gameManagerscript.currentlevel == 4 & gameManagerscript.currentPassenger == 7 ) //Check if dropoff prefab is not active, current level and current passenger is equal
+                {
+                    spawnManagerscript.PickupPoints[6].SetActive(false); //CTHM- pickup despawn
+                    spawnManagerscript.DropOffPoints[7].SetActive(true); //ULS - dropoff Spawn
+                    gameManagerscript.currentPassenger += 1; //currentpassenger = 8
+                }
+            
+            //9th passenger 
+            if(spawnManagerscript.DropOffPoints[7].activeSelf == false & gameManagerscript.currentlevel == 4 & gameManagerscript.currentPassenger == 8 ) //Check if dropoff prefab is not active, current level and current passenger is equal
+                {
+                    spawnManagerscript.PickupPoints[7].SetActive(false); //Gate 3- pickup despawn
+                    spawnManagerscript.DropOffPoints[8].SetActive(true); //Museo - dropoff Spawn
+                    gameManagerscript.currentPassenger += 1; //currentpassenger = 9
+                } 
+        }
+
+        if(gameManagerscript.currentlevel == 5)
+        {
+            //10th passenger
+            if(gameManagerscript.currentPassenger == 10)
+            {
+                spawnManagerscript.PickupPoints[8].SetActive(false); // JFH-pickup despawn
+                spawnManagerscript.DropOffPoints[9].SetActive(true); // Gate 3-dropoff spawn
+                
+                gameManagerscript.currentPassenger += 1; //currentpassenger = 10
+            }
+            
+            //11th passenger 
+            if(spawnManagerscript.DropOffPoints[9].activeSelf == false & gameManagerscript.currentlevel == 5 & gameManagerscript.currentPassenger == 10 ) //Check if dropoff prefab is not active, current level and current passenger is equal
+                {
+                    spawnManagerscript.PickupPoints[9].SetActive(false); //University pool- pickup despawn
+                    spawnManagerscript.DropOffPoints[10].SetActive(true); //PCH - dropoff Spawn
+                    gameManagerscript.currentPassenger += 1; //currentpassenger = 11
+                }
+            
+            //12th passenger 
+            if(spawnManagerscript.DropOffPoints[10].activeSelf == false & gameManagerscript.currentlevel == 5 & gameManagerscript.currentPassenger == 11 ) //Check if dropoff prefab is not active, current level and current passenger is equal
+                {
+                    spawnManagerscript.PickupPoints[10].SetActive(false); //FCH- pickup despawn
+                    spawnManagerscript.DropOffPoints[11].SetActive(true); //RCC - dropoff Spawn
+                    gameManagerscript.currentPassenger += 1; //currentpassenger = 12
+                } 
+            
+            //13th passenger 
+            if(spawnManagerscript.DropOffPoints[11].activeSelf == false & gameManagerscript.currentlevel == 5 & gameManagerscript.currentPassenger == 12 ) //Check if dropoff prefab is not active, current level and current passenger is equal
+                {
+                    spawnManagerscript.PickupPoints[11].SetActive(false); //ULS- pickup despawn
+                    spawnManagerscript.DropOffPoints[12].SetActive(true); //Mila's - dropoff Spawn
+                    gameManagerscript.currentPassenger += 1; //currentpassenger = 13
+                } 
+        }
+
+        if(gameManagerscript.currentlevel == 6)
+        {
+            //14th passenger
+            if(gameManagerscript.currentPassenger == 14)
+            {
+                spawnManagerscript.PickupPoints[7].SetActive(false); // Gate 3-pickup despawn
+                spawnManagerscript.DropOffPoints[13].SetActive(true); // Gate 2-dropoff spawn
+                
+                gameManagerscript.currentPassenger += 1; //currentpassenger = 14
+            }
+            
+            //15th passenger 
+            if(spawnManagerscript.DropOffPoints[13].activeSelf == false & gameManagerscript.currentlevel == 6 & gameManagerscript.currentPassenger == 14 ) //Check if dropoff prefab is not active, current level and current passenger is equal
+                {
+                    spawnManagerscript.PickupPoints[12].SetActive(false); //Mariano alvarez hall- pickup despawn
+                    spawnManagerscript.DropOffPoints[14].SetActive(true); //Grandstand - dropoff Spawn
+                    gameManagerscript.currentPassenger += 1; //currentpassenger = 15
+                }
+        }   
     }
     void OnTriggerExit(Collider col)
     {
