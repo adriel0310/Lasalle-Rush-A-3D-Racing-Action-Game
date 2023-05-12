5d06cb356dce4f099191551ad8111f06f4b924b6
janniehatesu <jannaarcullo@gmail.com>
2023-05-12 23:36:03 +0800

    + trees, waypoint&powerups models

---
+++ LaSalleRush-Prototype/Assets/Scripts/Powerups/Spinner.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spinner : MonoBehaviour
+{
+    public float spinSpeed = 80f;
+    public float floatHeight = 0.5f;
+    public float minFloatHeight = 0f;
+    public float floatSpeed = 4f;
+    public float floatAmplitude = 0.2f;
+
+    void Start()
+    {
+        floatHeight += transform.position.y; // Add the current Y position to the floatHeight variable
+        StartCoroutine(SpinAndFloat());
+    }
+
+    IEnumerator SpinAndFloat()
+    {
+        while (true)
+        {
+            // Spin the object around its X-axis
+            transform.Rotate(0f, 0f, spinSpeed * Time.deltaTime);
+
+            // Move the object up and down
+            float newY = Mathf.Sin(Time.time * floatSpeed) * floatAmplitude + floatHeight;
+
+            // Apply minimum float height
+            if (newY < minFloatHeight) {
+                newY = minFloatHeight;
+            }
+            transform.position = new Vector3(transform.position.x, newY, transform.position.z);
+
+            yield return null;
+        }
+    }
+}
