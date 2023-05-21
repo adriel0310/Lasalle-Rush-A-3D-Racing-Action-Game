b14898c3ffae18dcad751c8e71cd481d67c8d313
janniehatesu <jannaarcullo@gmail.com>
2023-05-22 05:05:13 +0800

    sfx slider (90% done)

--- LaSalleRush-Prototype/Assets/Scripts/AudioManager.cs
+++ LaSalleRush-Prototype/Assets/Scripts/AudioManager.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 using UnityEngine.UI;
 
 public class AudioManager: MonoBehaviour
@@ -13,23 +14,21 @@ public class AudioManager: MonoBehaviour
     [SerializeField] Slider volumeSlider; 
     [SerializeField] Slider sfxSlider; 
 
-    void Start(){
-        if(!PlayerPrefs.HasKey("musicVolume")){
-            PlayerPrefs.SetFloat("musicVolume",1);
-            Load();
-        }
-        else{
-            Load();
+    void Start()
+    {
+        if (!PlayerPrefs.HasKey("musicVolume"))
+        {
+            PlayerPrefs.SetFloat("musicVolume", 1);
+            BGLoad();
         }
 
-        if(!PlayerPrefs.HasKey("sfxVolume")){
-            PlayerPrefs.SetFloat("sfxVolume",1);
-            Load();
-        }
-        else{
-            Load();
+        if (!PlayerPrefs.HasKey("sfxVolume"))
+        {
+            PlayerPrefs.SetFloat("sfxVolume", 1);
+            SFXLoad();
         }
     }
+
 
     public void Initialize(AudioSource engine, AudioSource music)
     {
@@ -63,20 +62,26 @@ public class AudioManager: MonoBehaviour
 
     public void ChangeMusicVolume(){
         backgroundMusic.volume = volumeSlider.value;
-        Save();
+        BGSave();
     }
     public void ChangeSFXVolume(){
         engineSound.volume = sfxSlider.value;
-        Save();
+        SFXSave();
     }
 
-    private void Load(){
+    private void BGLoad(){
         volumeSlider.value = PlayerPrefs.GetFloat("musicVolume");
+    }
+
+    private void BGSave(){
+        PlayerPrefs.SetFloat("musicVolume", volumeSlider.value);
+    }
+
+    private void SFXLoad(){
         sfxSlider.value = PlayerPrefs.GetFloat("sfxVolume");
     }
 
-    private void Save(){
-        PlayerPrefs.SetFloat("musicVolume", volumeSlider.value);
+    private void SFXSave(){
         PlayerPrefs.SetFloat("sfxVolume", sfxSlider.value);
     }
 
--- LaSalleRush-Prototype/Assets/Scripts/UI Scripts/PauseScreen.cs
+++ LaSalleRush-Prototype/Assets/Scripts/UI Scripts/PauseScreen.cs
@@ -1,13 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+
 
 public class PauseScreen : MonoBehaviour
 {
     public GameObject pauseui;
     public GameObject optionsvolumeui;
     public GameObject optionscontrolsui;
-
 
     void Start()
     {
